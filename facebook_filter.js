a2a5667f72147654f729cbfd3f4b81d992c8e730
Marcio Toshio <contato@marciotoshio.com.br>
2012-01-14 00:43:32 -0200

    filter template

--- facebook_filter.js
+++ facebook_filter.js
@@ -6,10 +6,26 @@ root.addEventListener('DOMNodeInserted', function() {
 	}
 });
 
+var filteredStories = new Array();
+
 function filterByAuthor(story) {
-	var filters = ['Some Text to Block', 'Another Text to Block'];
+	var filters = ['Altas Risadas'];
 	var author = story.querySelector('.uiAttachmentDetails a');
-	if(author != null && filters.indexOf(author.innerText) > -1) {
-		story.innerText = "[ FILTERED ]";
+	if(author != null && filters.indexOf(author.innerText) > -1 && filteredStories.indexOf(story.id) < 0) {
+		filteredStories.push(story.id);
+		story.querySelector('.storyHighlightIndicatorWrapper').style.display = 'none';
+		story.querySelector('.storyContent').style.display = 'none';
+		story.appendChild(filteredTemplate());
 	}
+}
+
+function filteredTemplate() {
+	var div = document.createElement('div');
+	div.style.textAlign = 'center';
+	div.style.padding = '5px';
+	div.style.margin = '5px';
+	div.style.backgroundColor = '#FAA';
+	div.style.border = '1px solid #F00';
+	div.innerText = 'This content has been filtered by FacebookFilter';
+	return div;
 }
